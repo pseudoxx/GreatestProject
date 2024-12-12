3519ebeb0d4b611d4a705e46b42875a5b5db7250
pseudoxx <41193900+pseudoxx@users.noreply.github.com>
2024-12-12 11:01:25 +0000

    LSH I/O edited

--- LSH/main.py
+++ LSH/main.py
@@ -2,10 +2,14 @@ import pandas as pd
 import numpy as np
 
 def ReadData():
-    # Read data from txt file
-    data = pd.read_csv('test_articles.txt', header=None)
-    return data
-
-def
+    with open("LSH/test_articles.txt", 'r') as f:
+        data = f.readlines()
+    df = pd.DataFrame(data, columns=['raw_data'])
+    df[['ID', 'text']] = df['raw_data'].str.split(' ', n=1, expand=True)
+    df = df.drop('raw_data', axis=1)
+    df['text'] = df['text'].str[:-1]   
+    return df
 
 df = ReadData()
+# print the first line of df
+print(df.head(1).values[0])
