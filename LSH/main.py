cd4ded968503eb00b66d0f061cfd0b0639bfc0c3
pseudoxx <41193900+pseudoxx@users.noreply.github.com>
2024-12-12 07:05:24 +0000

    modified task 1

---
+++ LSH/main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+
+def ReadData():
+    # Read data from txt file
+    data = pd.read_csv('test_articles.txt', header=None)
+    return data
+
+def
+
+df = ReadData()
