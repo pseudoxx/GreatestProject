cd4678c3e67309bc5c1a22ecd53f2714055606d7
pseudoxx <41193900+pseudoxx@users.noreply.github.com>
2024-12-12 13:51:24 +0000

    data IO and preprocessing completed.

--- LSH/main.py
+++ LSH/main.py
@@ -7,9 +7,12 @@ def ReadData():
     df = pd.DataFrame(data, columns=['raw_data'])
     df[['ID', 'text']] = df['raw_data'].str.split(' ', n=1, expand=True)
     df = df.drop('raw_data', axis=1)
-    df['text'] = df['text'].str[:-1]   
+    df['text'] = df['text'].str[:-1]
+    df['text'] = df.text.str.replace("``", "", regex=True).replace("\'\'", "", regex=True).replace(" --", "", regex=True).replace(", ", " ", regex=True).replace(r"\. ", " ", regex=True).replace("  ", " ", regex=True).replace("\"", "", regex=True)
+    df['text'] = df['text'].str.lower()
     return df
 
 df = ReadData()
-# print the first line of df
+# data I/O output test
 print(df.head(1).values[0])
+
